46a52d8f7d9a3f26bc9e9a1e13c84717f19e37bf
kaem <konmarciniak@gmail.com>
2023-11-18 21:38:30 +0000

    Defining classes & Anonymous types

--- Start/Ch1/AnonTypes/Program.cs
+++ Start/Ch1/AnonTypes/Program.cs
@@ -2,12 +2,26 @@
 // Creating and using Anonymous types
 
 // TODO: Anonymous types can be declared using "new" and { }, similar to JS
-
+var myobj = new {
+    Name = "Konrad Marciniak",
+    Age = 25,
+    Adress = new {
+        Street = "123 Main St",
+        City = "Anywhere"
+    }
+};
+// Compiler infers the type of each property
+// Anonymous types also override the ToString method of the base Object class.
+Console.WriteLine($"{myobj.Name}, {myobj.Adress.Street}");
+Console.WriteLine($"{myobj}");
 
 // Anonymous types are read-only and cannot be modified
 // myobj.Name = "Jane Doe"; # will cause an error
 
 // TODO: To change a value, use non-destructive mutation and "with" clause
-
+var myobj2 = myobj with {Name = "Joe Marini"};
+Console.WriteLine($"{myobj2}");
 
 // TODO: You can check to see if an anonymous object contains a property
+Console.WriteLine($"{myobj.GetType().GetProperty("Name") != null}");
+Console.WriteLine($"{myobj.GetType().GetProperty("Job") != null}");
--- Start/Ch1/DefineClasses/shapes.cs
+++ Start/Ch1/DefineClasses/shapes.cs
@@ -2,15 +2,25 @@
 // Defining and instantiating classes
 
 // TODO: Define a class using the "class" keyword
-
+class Rectangle {
     // TODO: The constructor accepts parameters used to create the object
-
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
 
     // TODO: For convenience, we can have a constructor that takes one value
     // for squares that have the same side size
-
+    public Rectangle(int side) {
+        this.width = this.height = side;
+    }
 
     // TODO: Classes can define methods that return values
-
+    public int getArea() {
+        return this.width * this.height;
+    }
 
     // TODO: member variables hold data
+    public int width;
+    public int height;
+}
